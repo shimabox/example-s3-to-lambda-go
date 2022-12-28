0e3a4c9044d0201377ec4a62505487c6dc75c0fa
shimabox <shimabox@gmail.com>
2022-12-28 15:40:27 +0900

    :+1: 環境変数で読み込んでいれば明示して設定する必要はない

--- build/localstack/init.sh
+++ build/localstack/init.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
 #############################
-# aws configure set
-#############################
-aws configure set default.region "$AWS_DEFAULT_REGION"
-aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
-aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
-
-#############################
 # S3 initialize.
 #############################
 bash /docker-entrypoint-initaws.d/s3/make_bucket.sh
