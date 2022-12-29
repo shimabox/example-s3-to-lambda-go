83f9f74348519757f8c9b18286ab3c00df3bc96b
shimabox <shimabox@gmail.com>
2022-12-29 17:37:22 +0900

    :+1: localstackで実行するLambda関数に環境変数を渡す

--- scripts/localstack/lambda/update_function_configuration.sh
+++ scripts/localstack/lambda/update_function_configuration.sh
@@ -6,7 +6,11 @@ if [ $(exists_lambda "$LAMBDA_FUNC_NAME") -eq 1 ]; then
     aws lambda update-function-configuration \
         --endpoint-url="$ENDPOINT_URL" \
         --function-name "$LAMBDA_FUNC_NAME" \
-        --environment Variables={IS_LOCAL=true}
+        --environment "Variables={ \
+            IS_LOCAL=$IS_LOCAL, \
+            LOCALSTACK_URL=$LOCALSTACK_URL, \
+            AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
+          }"
 
     echo "Done lambda update-function-configuration $LAMBDA_FUNC_NAME."
 fi
