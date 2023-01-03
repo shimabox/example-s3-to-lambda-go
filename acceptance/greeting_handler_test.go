9a17d7f6a0a6037bf43cf7c4f77a37fd58c9da0e
shimabox <shimabox@gmail.com>
2023-01-03 17:31:57 +0900

    :green_heart: 受け入れテスト追加

---
+++ acceptance/greeting_handler_test.go
@@ -0,0 +1,40 @@
+package acceptance
+
+import (
+	"context"
+	"testing"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/google/go-cmp/cmp"
+
+	"shimabox/example-s3-to-lambda-go/app"
+	"shimabox/example-s3-to-lambda-go/testhelper"
+)
+
+func Test_Handler_挨拶が出力される(t *testing.T) {
+	registry := app.NewRegistry()
+	greetingRepository := registry.NewGreetingRepository()
+	greetingHandler := registry.NewGreetingHandler(greetingRepository)
+
+	key1 := "test.json"
+	s3Entity := events.S3Entity{
+		Object: events.S3Object{
+			Key: key1,
+		},
+	}
+	eventRecords := []events.S3EventRecord{
+		{
+			S3: s3Entity,
+		},
+	}
+	s3Event := events.S3Event{Records: eventRecords}
+
+	got := testhelper.ExtractStdout(t, func() {
+		greetingHandler.Handler(context.Background(), s3Event)
+	})
+	want := "山田太郎: こんにちは"
+
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("Handler is mismatch (-want +got):\n%s", diff)
+	}
+}
