8d05b5ffb35a3372f6f6a1bfa4ac3fbb579feb00
shimabox <shimabox@gmail.com>
2023-01-07 19:46:12 +0900

    :+1: S3をアップロードしたあとにsleepするように修正

--- scripts/localstack/helper/upload.sh
+++ scripts/localstack/helper/upload.sh
@@ -2,3 +2,9 @@
 
 aws s3 cp "$SCRIPT_DIR"/helper/test.json s3://"$S3_INPUT_BUCKET" \
     --endpoint-url="$ENDPOINT_URL"
+ret_code=${?}
+
+if [ "$ret_code" -eq 0 ]; then
+  sleep 3 # ログへの反映を待つ
+  echo "Put to S3 done."
+fi
--- scripts/localstack/operation_check.sh
+++ scripts/localstack/operation_check.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
 bash "$SCRIPT_DIR"/helper/upload.sh
-
-# TODO: logStreamNameが取れるまでループで確認する
-sleep 15 # 反映を待つ
-
 bash "$SCRIPT_DIR"/helper/get_log.sh
