e92fd061d29e3e4e45b2165d7283ad19a2228ae4
shimabox <shimabox@gmail.com>
2023-01-29 18:48:30 +0900

    :recycle: `--notification-configuration` jsonファイルからの読み込みをやめる

--- scripts/localstack/s3/add_notification_event.sh
+++ scripts/localstack/s3/add_notification_event.sh
@@ -3,4 +3,10 @@
 aws s3api put-bucket-notification-configuration \
     --bucket "$S3_INPUT_BUCKET" \
     --endpoint-url="$ENDPOINT_URL" \
-    --notification-configuration file://"$SCRIPT_DIR"/s3/s3_notification_config.json
+    --notification-configuration "{
+      \"LambdaFunctionConfigurations\": [
+        {
+          \"LambdaFunctionArn\": \"arn:aws:lambda:ap-northeast-1:000000000000:function:$LAMBDA_FUNC_NAME\",
+          \"Events\": [\"s3:ObjectCreated:Put\"]
+        }
+    }"
